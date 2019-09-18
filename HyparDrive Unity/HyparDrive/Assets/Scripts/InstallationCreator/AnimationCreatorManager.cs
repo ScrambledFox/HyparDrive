4421c06e68c5ad41640ca4d240d331e4136e8663
Joris Lodewijks <info@jorislodewijks.nl>
2019-09-18 22:35:18 +0200

    UI

--- HyparDrive Unity/HyparDrive/Assets/Scripts/InstallationCreator/AnimationCreatorManager.cs
+++ HyparDrive Unity/HyparDrive/Assets/Scripts/InstallationCreator/AnimationCreatorManager.cs
@@ -6,12 +6,21 @@ public class AnimationCreatorManager : MonoBehaviour {
 
     public static AnimationCreatorManager INSTANCE;
 
+    public delegate void OnAnimationTrackAdded ( Track track );
+    public event OnAnimationTrackAdded onAnimationTrackAdded;
+
     public List<Track> tracks;
 
     private void Awake () {
+        INSTANCE = this;
+
+        // Init list of tracks
         tracks = new List<Track>();
-        
-        //tracks.Add(new Track());
     }
 
+    public void AddNewTrack () {
+        Track track = new Track();
+        tracks.Add(track);
+        onAnimationTrackAdded?.Invoke(track);
+    }
 }
