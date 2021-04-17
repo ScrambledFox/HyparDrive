965bb4e904b869da4c6e2609278c5b8fa014c9a6
Joris Lodewijks <joris.lodewijks@hotmail.com>
2021-04-17 17:57:54 +0200

    Anims 1 and strobe 4

---
+++ HyparDrive Unity/HyparDrive/Assets/Scripts/Animations/Steam/Steam_StaticColour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam_StaticColour : MonoBehaviour
+{
+    public List<GameObject> towerLights = new List<GameObject>();
+    private List<LightObject> towerLightObjects = new List<LightObject>();
+
+    // Start is called before the first frame update
+    void Start () {
+        towerLights.Add(Instantiate(GetComponent<CustomAnimationController>().lightSpherePrefab, new Vector3(0f, 1f, 0), Quaternion.identity));
+
+        for (int i = 0; i < towerLights.Count; i++) {
+            towerLightObjects.Add(towerLights[i].GetComponent<LightObject>());
+        }
+
+        for (int i = 0; i < towerLightObjects.Count; i++) {
+            towerLightObjects[i].SetRadius(10f);
+            towerLightObjects[i].SetColor(INSTALLATION_CONFIG.PRIMARY_COLOUR);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animate();
+    }
+
+    private void animate () {
+        for (int i = 0; i < towerLights.Count; i++) {
+            towerLightObjects[i].SetColor(INSTALLATION_CONFIG.PRIMARY_COLOUR);
+        }
+    }
+
+    void OnDestroy () {
+        for (int i = 0; i < towerLights.Count; i++) {
+            Destroy(towerLights[i]);
+        }
+    }
+}
---
+++ HyparDrive Unity/HyparDrive/Assets/Scripts/Animations/Steam/Steam_Strobe.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam_Strobe : MonoBehaviour
+{
+    public List<GameObject> towerLights = new List<GameObject>();
+    private List<LightObject> towerLightObjects = new List<LightObject>();
+
+    private bool state = false;
+
+    private float timer = 1.0f;
+
+    // Start is called before the first frame update
+    void Start () {
+        towerLights.Add(Instantiate(GetComponent<CustomAnimationController>().lightSpherePrefab, new Vector3(0f, 1f, 0), Quaternion.identity));
+
+        for (int i = 0; i < towerLights.Count; i++) {
+            towerLightObjects.Add(towerLights[i].GetComponent<LightObject>());
+        }
+
+        for (int i = 0; i < towerLightObjects.Count; i++) {
+            towerLightObjects[i].SetRadius(10f);
+            towerLightObjects[i].SetColor(INSTALLATION_CONFIG.PRIMARY_COLOUR);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animate();
+    }
+
+    private void animate () {
+        timer -= Time.deltaTime * CustomAnimationController.speedMultiplier;
+
+        if (timer < 0) {
+            timer = 1.0f;
+            state = !state;
+        }
+
+        for (int i = 0; i < towerLights.Count; i++) {
+            towerLightObjects[i].SetColor(INSTALLATION_CONFIG.PRIMARY_COLOUR);
+
+            if (state) {
+                towerLights[i].transform.position = new Vector3(100f, 0f, 0f);
+            } else {
+                towerLights[i].transform.position = new Vector3(0f, 1f, 0f);
+            }
+        }
+    }
+
+    void OnDestroy () {
+        for (int i = 0; i < towerLights.Count; i++) {
+            Destroy(towerLights[i]);
+        }
+    }
+}
